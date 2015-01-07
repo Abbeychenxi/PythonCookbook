1640da0a122fa659ab03031f5337a01e70e45e30
陈希 <abbeychenx@gmail.com>
2015-01-07 15:37:03 +0800

    多个字符串替换

---
+++ make_xlat.py
@@ -0,0 +1,19 @@
+import re
+
+class make_xlat:
+    def __init__(self, *args, **kwds):
+        self.adict = dict(*args, **kwds)
+        self.rx = self.make_rx()
+
+    def make_rx(self):
+        return re.compile('|'.join(map(re.escape, self.adict)))
+
+    def one_xlat(self, match):
+        return self.adict[match.group(0)]
+
+    def __call__(self, text):
+        return self.rx.sub(self.one_xlat, text)
+
+class make_xlat_by_whole_words(make_xlat):
+    def make_rx(self):
+        return re.compile(r'\b%s\b' % r'\b|\b'.join(map(re.escape, self.adict)))
