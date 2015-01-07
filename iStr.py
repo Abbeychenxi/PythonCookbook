bac9cce40342bf52ab5934f2938826184eae0470
陈希 <abbeychenx@gmail.com>
2015-01-07 16:50:24 +0800

    让某些字符串大小写不敏感

---
+++ iStr.py
@@ -0,0 +1,38 @@
+#coding: utf-8
+class iStr(str):
+    """
+    大小写不敏感的字符串类
+    行为方式类似于str，只是所有的比较和查询
+    都是大小写不敏感的
+    """
+
+    def __init__(self, *args):
+        self._lowered = str.lower(self)
+    def __repr__(self):
+        return '%s(%s)' % (type(self).__name__, str.__repr__(self))
+    def __hash__(self):
+        return hash(self._lowered)
+    def lower(self):
+        return self._lowered
+
+def _make_case_insensitive(name):
+    '''将str的方法封装成istr的方法，大小写不敏感'''
+    str_meth = getattr(str, name)
+    def x(self, other, *args):
+        '''
+        先尝试将other小写化，通常这应该是一个字符串，
+        但必须要做好准备应对这个过程中出现的错误，
+        因为字符串是可以和非字符串正确地比较的
+        '''
+        try:
+            other = other.lower()
+        except (TypeError, AttributeError, ValueError):
+            pass
+        return str_meth(self._lowered, other, *args)
+        #x.func_name = name
+    setattr(iStr, name, x)
+for name in 'eq lt le gt gt ne contains'.split():
+    _make_case_insensitive('__%s__' % name)
+for name in 'count endswith find index rfind rindex startswith'.split():
+    _make_case_insensitive(name)
+del _make_case_insensitive
